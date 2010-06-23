f909aa5a8e2a64d79b35fb0ac2c08c2d146f8eeb
Muayyad Alsadi مؤيد السعدي <alsadi@ojuba.org>
2010-06-23 17:39:23 +0300

    fix mkdvd.sh

--- scripts/mkdvd.sh
+++ scripts/mkdvd.sh
@@ -5,9 +5,6 @@ pushd $(dirname "$0")/..
 [ -d  pungi_cache ] || mkdir pungi_cache
 
 pungi --name ojuba --ver $ojuba_release --flavor=Desktop --nosource \
-  --relnotefilere 'GPL README-BURNING-ISOS-en_US.txt ^RPM-GPG ^LICENSE' \
-  --relnotedirre '.*ojuba-documents' \
-  --relnotepkgs 'ojuba-samples ojuba-release ojuba-release-notes' \
   -c ks/oj/oj-install.ks --cachedir=$PWD/pungi_cache --destdir=$PWD/pungi_result 1>out.txt 2>err.txt
 
 popd
