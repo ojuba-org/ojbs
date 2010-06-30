28b89c48e9e60866ed99d3322b1695910ba7e5a8
Muayyad Alsadi مؤيد السعدي <alsadi@ojuba.org>
2010-06-30 10:44:47 +0300

    don't split dvd

--- scripts/mkdvd.sh
+++ scripts/mkdvd.sh
@@ -4,7 +4,7 @@ pushd $(dirname "$0")/..
 [ -d  pungi_result ] || mkdir pungi_result
 [ -d  pungi_cache ] || mkdir pungi_cache
 
-pungi --name ojuba --ver $ojuba_release --flavor=Desktop --nosource \
+pungi --nosplitmedia --nosource --name ojuba --ver $ojuba_release --flavor=Desktop \
   -c ks/oj/oj-install.ks --cachedir=$PWD/pungi_cache --destdir=$PWD/pungi_result 1>out.txt 2>err.txt
 
 popd
