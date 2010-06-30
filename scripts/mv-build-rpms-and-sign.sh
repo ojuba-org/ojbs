ba78af27cf057bf49c75e8ab49d71594a06ee156
Muayyad Alsadi مؤيد السعدي <alsadi@ojuba.org>
2010-06-30 10:59:38 +0300

    fix scripts/mv-build-rpms-and-sign.sh

--- scripts/mv-build-rpms-and-sign.sh
+++ scripts/mv-build-rpms-and-sign.sh
@@ -2,8 +2,8 @@
 pushd $(dirname "$0")/..
 . buildsys.conf
 [ -d  localrepos/.debug ] || mkdir localrepos/.debug
-find cache -name '*.rpm' -and -not '*-debuginfo.*.rpm' | xargs -r /bin/mv -t localrepos/cached
-find pungi* -name '*.rpm' -and -not '*-debuginfo.*.rpm' | xargs -r /bin/mv -t localrepos/cached
+find cache -name '*.rpm' -and -not -name '*-debuginfo.*.rpm' | xargs -r /bin/mv -t localrepos/cached
+find pungi* -name '*.rpm' -and -not -name '*-debuginfo.*.rpm' | xargs -r /bin/mv -t localrepos/cached
 rpm --resign localrepos/[^.]*/*.rpm
 
 popd
