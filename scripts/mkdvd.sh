d3526317a9c09cd46210b7dfae99bca2fe1a508c
Muayyad Alsadi مؤيد السعدي <alsadi@ojuba.org>
2011-08-11 20:44:39 +0300

    fix nosplitmedia

--- scripts/mkdvd.sh
+++ scripts/mkdvd.sh
@@ -4,7 +4,8 @@ pushd $(dirname "$0")/..
 [ -d  pungi_result ] || mkdir pungi_result
 [ -d  pungi_cache ] || mkdir pungi_cache
 
-time pungi --nosplitmedia --nosource --name ojuba --ver $ojuba_release --flavor=Desktop \
+#time pungi --nosplitmedia --nosource --name ojuba --ver $ojuba_release --flavor=Desktop \
+time pungi --nosource --name ojuba --ver $ojuba_release --flavor=Desktop \
   -c ks/oj/oj-install.ks --cachedir=$PWD/pungi_cache --destdir=$PWD/pungi_result 2>&1 | tee dvd-out-$(date +%Y-%m-%d).txt
 
 popd
