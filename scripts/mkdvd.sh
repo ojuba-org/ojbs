7f7858fb9e631ddc816b8752c4dcff8508f4e17b
Muayyad Alsadi مؤيد السعدي <alsadi@ojuba.org>
2011-08-11 20:39:16 +0300

    send output to file

--- scripts/mkdvd.sh
+++ scripts/mkdvd.sh
@@ -4,8 +4,8 @@ pushd $(dirname "$0")/..
 [ -d  pungi_result ] || mkdir pungi_result
 [ -d  pungi_cache ] || mkdir pungi_cache
 
-pungi --nosplitmedia --nosource --name ojuba --ver $ojuba_release --flavor=Desktop \
-  -c ks/oj/oj-install.ks --cachedir=$PWD/pungi_cache --destdir=$PWD/pungi_result 1>out.txt 2>err.txt
+time pungi --nosplitmedia --nosource --name ojuba --ver $ojuba_release --flavor=Desktop \
+  -c ks/oj/oj-install.ks --cachedir=$PWD/pungi_cache --destdir=$PWD/pungi_result 2>&1 | tee dvd-out-$(date +%Y-%m-%d).txt
 
 popd
 
