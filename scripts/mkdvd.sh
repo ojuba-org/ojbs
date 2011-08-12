07147e702ed4b512d61410fa4df0c63d61de83ab
Muayyad Alsadi مؤيد السعدي <alsadi@ojuba.org>
2011-08-12 14:48:41 +0300

    warn if dvd build already exists

--- scripts/mkdvd.sh
+++ scripts/mkdvd.sh
@@ -4,6 +4,8 @@ pushd $(dirname "$0")/..
 [ -d  pungi_result ] || mkdir pungi_result
 [ -d  pungi_cache ] || mkdir pungi_cache
 
+[ -d pungi_result/$ojuba_release/ ] && echo "directory pungi_result/$ojuba_release exists" && exit 1
+
 #time pungi --nosplitmedia --nosource --name ojuba --ver $ojuba_release --flavor=Desktop \
 time pungi --nosource --name ojuba --ver $ojuba_release --flavor=Desktop \
   -c ks/oj/oj-install.ks --cachedir=$PWD/pungi_cache --destdir=$PWD/pungi_result 2>&1 | tee dvd-out-$(date +%Y-%m-%d).txt
