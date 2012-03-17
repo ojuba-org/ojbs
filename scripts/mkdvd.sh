2f96dfbc98d3eff1386b8de1b0e3075138799c93
Muayyad Alsadi مؤيد السعدي <alsadi@ojuba.org>
2012-03-17 17:59:12 +0200

    report dvd rpm size

--- scripts/mkdvd.sh
+++ scripts/mkdvd.sh
@@ -7,8 +7,16 @@ pushd $(dirname "$0")/..
 [ -d pungi_result/$ojuba_release/ ] && echo "directory pungi_result/$ojuba_release exists" && exit 1
 
 #time pungi --nosplitmedia --nosource --name ojuba --ver $ojuba_release --flavor=Desktop \
-time pungi --nosource --name ojuba --ver $ojuba_release --flavor=Desktop \
-  -c ks/oj/oj-install.ks --cachedir=$PWD/pungi_cache --destdir=$PWD/pungi_result 2>&1 | tee dvd-out-$(date +%Y-%m-%d).txt
+$out=dvd-out-$(date +%Y-%m-%d).txt
+[ $is_testing -eq 1 ] && $args="" || $args="--isfinal"
+time pungi $args --nosource --name ojuba --ver $oj --flavor=Desktop \
+  -c ks/oj/oj-install.ks --cachedir=$PWD/pungi_cache --destdir=$PWD/pungi_result 2>&1 | tee $out
+
+cd pungi_result/$oj/Desktop/$base_arch/os/Packages && {
+echo "RPM FILES STARTS HERE:" >> $out
+du *.rpm | sort -r -n >> $out
+cd -
+}
 
 popd
 
